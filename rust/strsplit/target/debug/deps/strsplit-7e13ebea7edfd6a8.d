7babeb4b52b48ee370a1f0c84eef3b614d25b2a4
Pume Tuchinda <pumetuchinda@gmail.com>
2023-11-25 17:10:11 +0700

    remove as per gitignore

--- rust/strsplit/target/debug/deps/strsplit-7e13ebea7edfd6a8.d
+++
@@ -1,5 +0,0 @@
-/home/pumetu/random/rust/strsplit/target/debug/deps/strsplit-7e13ebea7edfd6a8.rmeta: src/lib.rs
-
-/home/pumetu/random/rust/strsplit/target/debug/deps/strsplit-7e13ebea7edfd6a8.d: src/lib.rs
-
-src/lib.rs:
--- rust/strsplit/target/debug/deps/strsplit-b1c78f8c7bb56930.d
+++
@@ -1,5 +0,0 @@
-/home/pumetu/random/rust/strsplit/target/debug/deps/strsplit-b1c78f8c7bb56930: src/lib.rs
-
-/home/pumetu/random/rust/strsplit/target/debug/deps/strsplit-b1c78f8c7bb56930.d: src/lib.rs
-
-src/lib.rs:
