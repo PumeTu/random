ff0636447978212e2042978396008e95d6f6d7ea
Pume Tuchinda <pumetuchinda@gmail.com>
2024-04-08 21:27:29 +0700

    remove rust book

--- rust/rust_book/minigrep/target/debug/deps/minigrep-05a48d3b2ebd3ef3.d
+++
@@ -1,5 +0,0 @@
-/Users/pumetuchinda/random/rust/rust_book/minigrep/target/debug/deps/minigrep-05a48d3b2ebd3ef3.rmeta: src/main.rs
-
-/Users/pumetuchinda/random/rust/rust_book/minigrep/target/debug/deps/minigrep-05a48d3b2ebd3ef3.d: src/main.rs
-
-src/main.rs:
--- rust/rust_book/minigrep/target/debug/deps/minigrep-115e98db7ea4d33d.d
+++
@@ -1,5 +0,0 @@
-/Users/pumetuchinda/random/rust/rust_book/minigrep/target/debug/deps/minigrep-115e98db7ea4d33d.rmeta: src/main.rs
-
-/Users/pumetuchinda/random/rust/rust_book/minigrep/target/debug/deps/minigrep-115e98db7ea4d33d.d: src/main.rs
-
-src/main.rs:
