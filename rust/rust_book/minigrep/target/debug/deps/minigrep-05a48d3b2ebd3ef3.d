e05bc8efdb8c351be4be5190499c1c58f966158f
Pume Tuchinda <pumetuchinda@Pumes-MacBook-Air.local>
2024-04-08 21:24:08 +0700

    init minigrep

---
+++ rust/rust_book/minigrep/target/debug/deps/minigrep-05a48d3b2ebd3ef3.d
@@ -0,0 +1,5 @@
+/Users/pumetuchinda/random/rust/rust_book/minigrep/target/debug/deps/minigrep-05a48d3b2ebd3ef3.rmeta: src/main.rs
+
+/Users/pumetuchinda/random/rust/rust_book/minigrep/target/debug/deps/minigrep-05a48d3b2ebd3ef3.d: src/main.rs
+
+src/main.rs:
