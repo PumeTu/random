75422d2b6aec251cebb7b4315f9d42d9f2d5fa0b
Pume Tuchinda <pumetuchinda@gmail.com>
2024-04-09 08:30:30 +0700

    initial impl of minigrep

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,3 +1,30 @@
+use std::{env, fs};
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Self, &'static str> {
+        // we use build instead of new because it is often
+        // expected that new will not fail
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        Ok(Self {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args);
+
+    let content =
+        fs::read_to_string(config.file_path).expect("Unreadable file, please enter a valid file");
+
+    println!("{content}")
 }
