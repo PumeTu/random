63d18d4ee22818f634479145a637b5528c6a4f5b
Pume Tuchinda <pumetuchinda@gmail.com>
2024-04-09 18:38:16 +0700

    update error to go into stderr

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -4,12 +4,12 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Error parsing the arguments: {err}");
+        eprintln!("Error parsing the arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Error running minigrep {e}");
+        eprintln!("Error running minigrep {e}");
         process::exit(1);
     }
 }
