483002994168c32ee0665f334acf2794a3f168de
Pume Tuchinda <pumetuchinda@gmail.com>
2024-04-17 16:53:19 +0700

    add infinite test

--- rust/iterators/src/lib.rs
+++ rust/iterators/src/lib.rs
@@ -141,4 +141,12 @@ mod tests {
         assert_eq!(iter.next(), None);
         assert_eq!(iter.next_back(), None);
     }
+
+    #[test]
+    fn inf() {
+        let mut iter = flatten((0..).map(|i| 0..i));
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), Some(1));
+    }
 }
